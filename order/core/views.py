f650fdd75a3c9e19975aaea1c23f902b3871f31d
saviosabino <saviopp@gmail.com>
2014-06-26 13:46:34 -0300

    ensure cookie csrftoken

--- order/core/views.py
+++ order/core/views.py
@@ -1,8 +1,10 @@
 from order.core import models
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
+from django.views.decorators.csrf import ensure_csrf_cookie
 import datetime
 
+@ensure_csrf_cookie
 def index(request):
     return render_to_response('index.html', {},
         context_instance=RequestContext(request)) 
